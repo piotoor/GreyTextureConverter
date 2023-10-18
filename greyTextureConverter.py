63f0d2d6336cd1fe12bc697046bfa9e74119ba04
Piotr Kozka <piotoor@gmail.com>
2023-10-18 14:12:29 +0200

    Implemented FLI-2 texture conversions

--- greyTextureConverter.py
+++ greyTextureConverter.py
@@ -8,6 +8,76 @@ import argparse
 DST_EXTENSION = ".asm"
 
 
+def convert_image_to_asm_code_fli_80x100(input_img):
+    cols = 32
+    rows = 52
+    pixels = np.array(list(input_img.getdata())).reshape((rows, cols))
+    print("pixels raw = {}".format(pixels))
+    cols_0_7 = []
+    cols_8_15 = []
+    cols_16_23 = []
+    cols_24_31 = []
+
+    for c in range(0, cols, 2):
+        for r in range(0, rows):
+            if c < 8:
+                cols_0_7.append((pixels[r][c] << 4) | pixels[r][c + 1])
+            elif c < 16:
+                cols_8_15.append((pixels[r][c] << 4) | pixels[r][c + 1])
+            elif c < 24:
+                cols_16_23.append((pixels[r][c] << 4) | pixels[r][c + 1])
+            else:
+                cols_24_31.append((pixels[r][c] << 4) | pixels[r][c + 1])
+
+    cols_0_7_asm = ".byte\t" + ",".join(map(str, cols_0_7[0:52])) + "\n.byte\t" + ",".join(map(str, cols_0_7[52:104])) + "\n.byte\t" + ",".join(map(str, cols_0_7[104:156])) + "\n.byte\t" + ",".join(map(str, cols_0_7[156:208]))
+    cols_8_15_asm = ".byte\t" + ",".join(map(str, cols_8_15)) + "\n.byte\t" + ",".join(map(str, cols_8_15[52:104])) + "\n.byte\t" + ",".join(map(str, cols_8_15[104:156])) + "\n.byte\t" + ",".join(map(str, cols_8_15[156:208]))
+    cols_16_23_asm = ".byte\t" + ",".join(map(str, cols_16_23)) + "\n.byte\t" + ",".join(map(str, cols_16_23[52:104])) + "\n.byte\t" + ",".join(map(str, cols_16_23[104:156])) + "\n.byte\t" + ",".join(map(str, cols_16_23[156:208]))
+    cols_24_31_asm = ".byte\t" + ",".join(map(str, cols_24_31)) + "\n.byte\t" + ",".join(map(str, cols_24_31[52:104])) + "\n.byte\t" + ",".join(map(str, cols_24_31[104:156])) + "\n.byte\t" + ",".join(map(str, cols_24_31[156:208]))
+
+    print(cols_0_7_asm)
+    print()
+    print(cols_8_15_asm)
+    print()
+    print(cols_16_23_asm)
+    print()
+    print(cols_24_31_asm)
+    print()
+
+    return cols_0_7_asm, cols_8_15_asm, cols_16_23_asm, cols_24_31_asm
+
+
+def save_image_to_asm_files_fli_80x100(file_path):
+    print("Processing {}".format(file_path))
+    curr_img = Image.open(file_path)
+    head, tail = os.path.split(file_path)
+    src_file_name, ext = os.path.splitext(tail)
+
+    a, b, c, d = convert_image_to_asm_code_fli_80x100(curr_img)
+    a_file_path = os.path.join(head, "..", src_file_name + "_a") + DST_EXTENSION
+    with open(a_file_path, 'w') as asm_file:
+        print("Saving {}...".format(a_file_path))
+        screen_buffer_output = "".join(a)
+        asm_file.write(screen_buffer_output)
+
+    b_file_path = os.path.join(head, "..", src_file_name + "_b") + DST_EXTENSION
+    with open(b_file_path, 'w') as asm_file:
+        print("Saving {}...".format(b_file_path))
+        screen_buffer_output = "".join(b)
+        asm_file.write(screen_buffer_output)
+
+    c_file_path = os.path.join(head, "..", src_file_name + "_c") + DST_EXTENSION
+    with open(c_file_path, 'w') as asm_file:
+        print("Saving {}...".format(c_file_path))
+        screen_buffer_output = "".join(c)
+        asm_file.write(screen_buffer_output)
+
+    d_file_path = os.path.join(head, "..", src_file_name + "_d") + DST_EXTENSION
+    with open(d_file_path, 'w') as asm_file:
+        print("Saving {}...".format(d_file_path))
+        screen_buffer_output = "".join(d)
+        asm_file.write(screen_buffer_output)
+
+
 def convert_image_to_asm_code_80x50(input_img, full_size, key_door_columns):
     cols = 38 if key_door_columns else 32
     pixels = np.array(list(input_img.getdata())).reshape((50 if full_size else 26, cols))[:26]
@@ -83,6 +153,10 @@ def save_images_to_asm_files(textures_dir, extension, full_size):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('-F', '--fli',
+                        required=False,
+                        dest='fli',
+                        action='store_true')
     parser.add_argument('-q', '--hi-res',
                         required=False,
                         dest='hires',
@@ -107,6 +181,8 @@ if __name__ == '__main__':
             save_image_to_asm_files_80x50(args.path, args.full_size, args.key_door_columns)
         else:
             save_images_to_asm_files_80x50(args.path, "tga", args.full_size, args.key_door_columns)
+    elif args.fli:
+        save_image_to_asm_files_fli_80x100(args.path)
     else:
         if args.single:
             save_image_to_asm_files(args.path, args.full_size)
